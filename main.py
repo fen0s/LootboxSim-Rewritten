c27da2b40622362261aba9bbbe7d73d44244ab78
fen0s <37670363+fen0s@users.noreply.github.com>
2020-06-12 16:56:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from systems.application import Application
 from systems.engine import Engine
 from systems.inventory import Inventory
-engine = Engine()
-inventory = Inventory(engine)
-main_application = Application(engine, inventory)
+
+if __name__ == '__main__':
+    engine = Engine()
+    inventory = Inventory(engine)
+    main_application = Application(engine, inventory)
